9f9d18205ec9acefabd27d114051db9c47970134
Matej Otcenas <matejotcenas@localhost.localdomain>
2019-11-14 13:30:26 +0100

    Editing

--- xotcen01-fit-ili.sh
+++ xotcen01-fit-ili.sh
@@ -19,8 +19,8 @@ echo "To print the loop device generated using the above command use \"losetup-a
 ###### Creating RAID0 and RAID1 ######
 echo "----------------------------------------------"
 echo "Creating RAID0 ..."
-mdadm --create /dev/md1 --level=1 --raid-devices=2 /dev/loop0 /dev/loop1
-mdadm --create /dev/md0 --level=0 --raid-devices=2 /dev/loop2 /dev/loop3
+mdadm --create --verbose /dev/md1 --level=1 --raid-devices=2 /dev/loop0 /dev/loop1
+mdadm --create --verbose /dev/md0 --level=0 --raid-devices=2 /dev/loop2 /dev/loop3
 echo "Creating RAID1 ..."
 #mdadm --create /dev/md1 --level=1 --raid-devices=2 /dev/loop0 /dev/loop1
 echo "You can examine the created raid devices in detail using \"mdadm --detail /dev/md_number\" "
@@ -39,17 +39,17 @@ lvcreate FIT_vg -n FIT_1v2 -L100M
 ##### Creating EXT4 FS #####
 echo "-----------------------------------------------"
 echo "Creating EXT4 FS on FIT_1v1 logical volume ..."
-mkfs.ext4 /dev/FIT_1v1
+mkfs.ext4 /dev/FIT_vg/FIT_1v1
 
 ##### Creating XFS #####
 echo "------------------------------------------------"
 echo "Creating XFS on FIT_1v2 logical volume ..."
-mkfs.xfs /dev/FIT_1v2
+mkfs.xfs /dev/FIT_vg/FIT_1v2
 
 ##### Mounting FIT_1v1 to /mnt/test1 #####
 echo "------------------------------------------------"
 echo "Creating directories test1 test2 in /mnt..."
-mkdir -v /mnt/test1 /mnt/test2
+mkdir -p -v /mnt/test1 /mnt/test2
 echo "Mounting FIT_1v1 to /mnt/test1 ..."
-mount /dev/FIT_1v1 /mnt/test1
-mount /dev/FIT_1v2 /mnt/test2
+mount /dev/FIT_vg/FIT_1v1 /mnt/test1
+mount /dev/FIT_vg/FIT_1v2 /mnt/test2
