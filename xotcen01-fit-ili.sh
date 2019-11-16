117114283be4b5e4f8bd513faa266a9f8c37f7a2
Matej Otčenáš <mato.otcenas@gmail.com>
2019-11-16 13:58:21 +0100

    Update xotcen01-fit-ili.sh

--- xotcen01-fit-ili.sh
+++ xotcen01-fit-ili.sh
@@ -33,7 +33,7 @@ echo "Creating RAID0 ..."
 mdadm --create /dev/md0 --level=stripe --raid-devices=2 /dev/loop2 /dev/loop3 #RAID0
 
 echo "Creating RAID1 ..."
-mdadm --create /dev/md1 --level=mirror --raid-devices=2 /dev/loop0 /dev/loop1 #RAID1
+mdadm --create /dev/md1 --level=mirror - R --raid-devices=2 /dev/loop0 /dev/loop1 #RAID1
 
 echo "Showing changes ..."
 #mdadm --detail /dev/md0 /dev/md1
